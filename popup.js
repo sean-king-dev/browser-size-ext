148fe78a77a36f57403de30a00e7eed8231a6faf
Sean King <sean.king@kingseducation.com>
2024-10-01 12:44:37 +0100

    init repo

---
+++ popup.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const widthSpan = document.getElementById('width');
+    const heightSpan = document.getElementById('height');
+  
+    // Get the active tab and then execute the script
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      chrome.scripting.executeScript({
+        target: {tabId: tabs[0].id},
+        func: () => {
+          return {
+            width: window.innerWidth,
+            height: window.innerHeight
+          };
+        }
+      }, (results) => {
+        if (results && results[0]) {
+          const {width, height} = results[0].result;
+          widthSpan.textContent = width;
+          heightSpan.textContent = height;
+        }
+      });
+    });
+  });
+  
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const widthSpan = document.getElementById('width');
+//     const heightSpan = document.getElementById('height');
+  
+//     // Get the current tab's content dimensions
+//     chrome.scripting.executeScript({
+//       target: {tabId: chrome.tabs.TAB_ID_NONE},
+//       func: () => {
+//         return {
+//           width: window.innerWidth,
+//           height: window.innerHeight
+//         };
+//       }
+//     }, (results) => {
+//       if (results && results[0]) {
+//         const {width, height} = results[0].result;
+//         widthSpan.textContent = width;
+//         heightSpan.textContent = height;
+//       }
+//     });
+//   });
+  
